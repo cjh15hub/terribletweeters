0abd22c278809483e50489c8b98d8900f2f89a5f
ryanc16 <rconklin1994@yahoo.com>
2021-06-09 23:44:43 -0400

    Create IDamagable interface and Entity class which can be added to game objects which should have health and take damage.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using UnityEngine;
+
+public class Entity : MonoBehaviour, IDamagable
+{
+    [SerializeField]
+    public float initialHealth = 100;
+    public float health { get { return _health; } set { _health = value; } }
+    [SerializeField]
+    private float _health = 100;
+
+    [SerializeField]
+    private float destroyDelay = 1.5f;
+
+    public bool isDead { get => health <= 0 && _dead; }
+    private bool _dead = false;
+
+    // component references
+    private Accelerometer accelerometer;
+
+    private void Start()
+    {
+        _health = initialHealth;
+        accelerometer = GetComponent<Accelerometer>();
+        if (accelerometer == null)
+        {
+            accelerometer = gameObject.AddComponent(typeof(Accelerometer)) as Accelerometer;
+        }
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Entity otherEntity = collision.gameObject.GetComponent<Entity>();
+        Accelerometer otherAccelerometer = collision.gameObject.GetComponent<Accelerometer>();
+        if (otherAccelerometer)
+        {
+            TakeDamage(otherAccelerometer.impactForce.magnitude);
+            if(otherEntity)
+            {
+                otherEntity.TakeDamage(accelerometer.impactForce.magnitude);
+            }
+        }
+        TakeDamage(accelerometer.impactForce.magnitude);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    public void Die()
+    {
+        if (!_dead)
+        {
+            health = 0;
+            DeathRoutine();
+            StartCoroutine(DeactivateAfterDelay());
+            _dead = true;
+        }
+    }
+
+    public virtual void DeathRoutine(){}
+
+    private IEnumerator DeactivateAfterDelay()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        this.gameObject.SetActive(false);
+    }
+}
