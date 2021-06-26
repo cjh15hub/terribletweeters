f584bd092ddc964567f49778b9928654118e260e
CharlesHaines <cjh15@pct.edu>
2021-06-25 22:14:50 -0400

    Improve Camera Bird Script
    Improve Camera Bird Script to calculate offScreen positions

--- Assets/Scripts/Menu/MenuBird.cs
+++ Assets/Scripts/Menu/MenuBird.cs
@@ -6,13 +6,24 @@ public class MenuBird : MonoBehaviour
 {
     public Vector2 velocity = new Vector2(2.0f, 0);
 
-    private float screenEnds = 20;
+    // camera values
+    private Camera mainCamera;
+    private float offScreenLeft = 0;
+    private float offScreenRight = 0;
+    private float offScreenPadding = 6;
 
+    public void Awake()
+    {
+        mainCamera = Camera.main;
+
+        offScreenLeft = mainCamera.ScreenToWorldPoint(Vector3.zero).x - offScreenPadding;
+        offScreenRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x + offScreenPadding;
+    }
 
     public void Update()
     {
-        if (transform.position.x > screenEnds) transform.position = new Vector2(-1 * screenEnds, transform.position.y);
-        if (transform.position.x < -1 * screenEnds) transform.position = new Vector2(screenEnds, transform.position.y);
+        if (transform.position.x > offScreenRight) transform.position = new Vector2(offScreenLeft, transform.position.y);
+        else if (transform.position.x < offScreenLeft) transform.position = new Vector2(offScreenRight, transform.position.y);
 
         transform.Translate(velocity * Time.deltaTime);
     }
