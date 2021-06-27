d0b336054171ee6703eafd038b0c52966c0488f9
CharlesHaines <cjh15@pct.edu>
2021-06-27 00:21:02 -0400

    Add Level Complete Menu - G001F03
    - Add Level Complete Mode to Pause Menu
    - Update Level Controller to open Complete Pause Menu when all monsters are dead
    - Improve level unload and load logic

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -1,14 +1,25 @@
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject PausePanel, PauseButton, ResumeButton, RestartButton, SettingsButton, ExitButton;
+    public GameObject PauseButton, PausePanel, PauseMessage, SettingsButton, LevelsButton, RestartButton, ResumeButton;
+    public GameObject CompleteMessage, LevelName, Score, ContinueButton; 
+
+    private LevelController LevelController;
+
+    public void Awake()
+    {
+        // Turn Panel off, Pause Button On
+        PausePanel.SetActive(false);
+        PauseButton.SetActive(true);
+    }
 
     public void Start()
     {
-        
+        LevelController = FindObjectOfType<LevelController>();
     }
 
     public void Update()
@@ -20,28 +31,71 @@ public class PauseMenu : MonoBehaviour
     }
     public void Pause()
     {
+        // Turn Panel on, Pause Button Off
         PausePanel.SetActive(true);
         PauseButton.SetActive(false);
+
+        // Make Sure Pause UI is on
+        PauseMessage.SetActive(true);
+        ResumeButton.SetActive(true);
+
+        // Make Sure Complete UI is off
+        CompleteMessage.SetActive(false);
+        LevelName.SetActive(false);
+        Score.SetActive(false);
+        ContinueButton.SetActive(false);
     }
 
     public void Resume()
     {
+        // Turn Panel off, Pause Button On
+        PausePanel.SetActive(false);
+        PauseButton.SetActive(true);
+    }
+
+    public void LevelStats(string levelName, bool complete, int score)
+    {
+        // Turn Panel on, Pause Button Off
+        PausePanel.SetActive(true);
+        PauseButton.SetActive(false);
+
+        // Make Sure Complete UI is on
+        CompleteMessage.SetActive(true);
+        LevelName.SetActive(true);
+        Score.SetActive(true);
+        ContinueButton.SetActive(true);
+
+        // Make Sure Pause UI is off
+        PauseMessage.SetActive(false);
+        ResumeButton.SetActive(false);
+
+        // Set Labels
+        LevelName.GetComponent<TextMeshProUGUI>().SetText(levelName);
+        Score.GetComponent<TextMeshProUGUI>().SetText(score.ToString());
+    }
+
+    public void Continue()
+    {
+        LevelController.GoToNextLevel();
+
+        // Turn Panel off, Pause Button On
         PausePanel.SetActive(false);
         PauseButton.SetActive(true);
     }
 
     public void Restart()
     {
-        FindObjectOfType<LevelController>().ReloadLevel();
+        LevelController.RestartLevel();
+        Resume();
     }
 
     public void Exit()
     {
-        FindObjectOfType<LevelController>().LoadStartMenu();
+        LevelController.LoadStartMenu();
     }
 
     public void Levels()
     {
-        FindObjectOfType<LevelController>().LoadLevelSelection();
+        LevelController.LoadLevelSelection();
     }
 }
